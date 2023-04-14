ab1e6ac2334618f3b1168bd8115229bf04ee5151
quyngo <quyngo555@gmail.com>
2023-04-14 11:31:55 +0700

    update apartment, contract, person

--- src/main/java/com/vmo/apartment_manager/controller/mailController.java
+++ src/main/java/com/vmo/apartment_manager/controller/mailController.java
@@ -1,23 +1,23 @@
-package com.vmo.apartment_manager.controller;
-
-import com.vmo.apartment_manager.entity.Bill;
-import com.vmo.apartment_manager.repository.BillRepository;
-import com.vmo.apartment_manager.service.BillService;
-import com.vmo.apartment_manager.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class mailController {
-  @Autowired
-  EmailService emailService;
-  @PostMapping("/api/mail")
-  public ResponseEntity<?> sendMail(Bill bill){
-
-    return ResponseEntity.ok(emailService.sendMail(bill));
-  }
-}
+//package com.vmo.apartment_manager.controller;
+//
+//import com.vmo.apartment_manager.entity.Bill;
+//import com.vmo.apartment_manager.repository.BillRepository;
+//import com.vmo.apartment_manager.service.BillService;
+//import com.vmo.apartment_manager.service.EmailService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class mailController {
+//  @Autowired
+//  EmailService emailService;
+//  @PostMapping("/api/mail")
+//  public ResponseEntity<?> sendMail(Bill bill){
+//
+//    return ResponseEntity.ok(emailService.sendMail(bill));
+//  }
+//}

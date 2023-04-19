0d5e55d36e7502ad32fa7567aaaeb2b3a8357793
quyngo <quyngo555@gmail.com>
2023-04-19 16:15:04 +0700

    update

--- src/main/java/com/vmo/apartment_manager/constant/ConstantError.java
+++ src/main/java/com/vmo/apartment_manager/constant/ConstantError.java
@@ -2,6 +2,7 @@ package com.vmo.apartment_manager.constant;
 
 public class ConstantError {
   public static final String APARTMENT_NOT_FOUND = "does not exist apartment with id: ";
+
   public static final String PERSON_NOT_FOUND = "does not exist resident with id: ";
   public static final String LACK_OF_EMAIL_PHONE = "lack of field email and phone";
   public static final String SERVICE_EXISTS = "Can't add 2 same services in 1 bill";
@@ -9,5 +10,8 @@ public class ConstantError {
   public static final String EXIT_REPRESENT = "represent is already in apartment ";
   public static final String CONTRACT_NOT_FOUND = "does not exist contract with id: ";
   public static final String CONTRACT_EXISTS = "Apartment is already in use, cannot add new contract";
+  public static final String CONTRACT_NOT_EXISTS_IN_APARTMENT = "Contract is not exists in Apartment : ";
+  public static final String FILE_IMPORT_ERROR = "File import error at: ";
+
 
 }

c7384060b92cd1c773615fd9521e1e297dc66c65
quyngo <quyngo555@gmail.com>
2023-05-04 21:45:41 +0700

    update

--- src/main/java/com/vmo/apartment_manager/payload/response/BillResponse.java
+++ src/main/java/com/vmo/apartment_manager/payload/response/BillResponse.java
@@ -20,6 +20,7 @@ public class BillResponse implements Serializable {
   private List<BillDetailResponse> billDetails;
   private String  apartmentName;
   private String createdBy;
+  private Date createdDate;
 
   public BillResponse(Bill bill) {
     this.id = bill.getId();
@@ -28,6 +29,7 @@ public class BillResponse implements Serializable {
     this.stauts = bill.getStauts();
     this.paidDate = bill.getPaidDate();
     this.note = bill.getNote();
+    this.createdDate = bill.getPaidDate();
     this.billDetails = bill.getBillDetailList().stream().map(BillDetailResponse::new).toList();
     this.apartmentName = bill.getContract().getApartment().getName();
   }
